299e44962f0623f84781964738fbbda4139a984e
sadness52r <111510546+sadness52r@users.noreply.github.com>
2023-12-18 10:50:08 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from config import *
-import Psycopg2Test
-import SQLiteTest
-import PandasTest
-
-print('Psycopg2 times:', Psycopg2Test.RunQueries())
-print('SQLite times:', SQLiteTest.RunQueries())
-print('Pandas times:', PandasTest.RunQueries())
